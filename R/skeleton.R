1fade5c3d4a5548b19fdf02555aba8c5f02a1e9b
JBrandon Duck-Mayr <j.duckmayr@gmail.com>
2022-03-14 11:03:24 -0500

    Add several options to project creation dialog
    Specifically, users can now set project info, including:
    - who the authors are
    - what the title is
    - and whether it's an empirical or formal theory project (or both)
    - (FIXME: get input from qual scholars on what they'd need)
    
    They can also specify a bib file to copy to their new project directory,
    and can select a few options regarding git:
    - whether to initiate a git repo for the new project (default yes)
    - whether to ignore PDF output in .gitignore (default yes)
    - whether to ignore binary data files in .gitignore (default yes)
    
    Finally, they can specify whether to have a separate 'analysis'
    subdirectory to break some of their code out into (a) separate file(s).
    Generally this should be discouraged, but it can be appropriate in some
    settings (this is discussed in the Rmd file copied to the 'analysis'
    subdirectory if it is created).

--- R/skeleton.R
+++ R/skeleton.R
@@ -1,13 +1,106 @@
-research_project_skeleton <- function(path, ...) {
+replace_file_text <- function(filepath, pattern, replacement) {
+    txt <- readLines(normalizePath(filepath))
+    txt <- gsub(pattern = pattern, replacement = replacement, x = txt)
+    writeLines(txt, con = normalizePath(filepath))
+}
+
+research_project_skeleton <- function(
+    path,
+    author,
+    title,
+    git,
+    ignore_pdf,
+    ignore_binary_data,
+    separate_analysis,
+    bib,
+    type,
+    ...
+) {
     dir.create(path)
     resources_path <- system.file(
         "rstudio", "templates", "project", "resources",
         package = "quack"
     )
-    resource_files <- list.files(path = resources_path)
     file.copy(
-        from = file.path(resources_path, resource_files),
-        to   = file.path(normalizePath(path), resource_files)
+        from = file.path(resources_path, "README.md"),
+        to   = file.path(normalizePath(path), "README.md")
     )
+    if ( git ) {
+        gert::git_init(path)
+    }
+    gitignore <- file.path(path, ".gitignore")
+    if ( ignore_pdf ) {
+        cat("## Ignore PDF output\n*.pdf\n\n", file = gitignore, append = TRUE)
+    }
+    if ( ignore_binary_data ) {
+        cat(
+            "## Ignore binary data\n*.rds\n*.RData\n\n",
+            file = gitignore, append = TRUE
+        )
+    }
+    cat(
+        "## Ignore user-specific files\n.Rhistory\n.RData\n.Ruserdata\n",
+        file = gitignore, append = TRUE
+    )
+    empirical <- type %in% c("Empirical", "Both")
+    if ( empirical ) {
+        dir.create(file.path(path, "data"))
+        file.copy(
+            from = file.path(resources_path, "data"),
+            to   = file.path(normalizePath(path)),
+            recursive = TRUE
+        )
+    }
+    if ( separate_analysis ) {
+        dir.create(file.path(path, "analysis"))
+        file.copy(
+            from = file.path(resources_path, "analysis"),
+            to   = file.path(normalizePath(path)),
+            recursive = TRUE
+        )
+    }
+    if ( author != "" ) {
+        if ( empirical ) {
+            replace_file_text(
+                filepath    = file.path(path, "data", "codebook.Rmd"),
+                pattern     = "Your Names\\(s\\) Here",
+                replacement = author
+            )
+        }
+        if ( separate_analysis ) {
+            replace_file_text(
+                filepath    = file.path(path, "analysis", "analysis.Rmd"),
+                pattern     = "Your Names\\(s\\) Here",
+                replacement = author
+            )
+        }
+    }
+    if ( title != "" ) {
+        replace_file_text(
+            filepath    = file.path(path, "README.md"),
+            pattern     = "^# Title Here",
+            replacement = paste("#", title)
+        )
+        if ( empirical ) {
+            replace_file_text(
+                filepath    = file.path(path, "data", "codebook.Rmd"),
+                pattern     = "Your Paper Title Here",
+                replacement = title
+            )
+        }
+        if ( separate_analysis ) {
+            replace_file_text(
+                filepath    = file.path(path, "analysis", "analysis.Rmd"),
+                pattern     = "Your Paper Title Here",
+                replacement = title
+            )
+        }
+    }
+    if ( bib != "" ) {
+        file.copy(
+            from = bib,
+            to   = file.path(normalizePath(path), basename(bib))
+        )
+    }
     return(TRUE)
 }
